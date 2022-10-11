273ec270ef1678c74f3e05e267064a4327d73a84
Bruno Rodrigues <30496075+bsrodrigs@users.noreply.github.com>
2022-10-11 15:04:38 +0100

    Update runner-instance-config.sh

--- runner-instance-config.sh
+++ runner-instance-config.sh
@@ -5,5 +5,5 @@
 
 sudo mkdir -p /opt/gitlab-runner/{metadata,builds,cache}
 curl -s "https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh" | sudo bash
-sudo apt install gitlab-runner
-sudo gitlab-runner register --url https://gitlab.com/ --registration-token $1 --name fargate-test-runner --run-untagged --executor custom -n
+sudo apt install gitlab-runner >> user_data.log
+sudo gitlab-runner register --url https://gitlab.com/ --registration-token $1 --name fargate-test-runner --run-untagged --executor custom -n >> user_data.log
